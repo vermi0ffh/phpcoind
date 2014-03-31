029849530fd2c9288f80f01142740a22cefef0d9
Vermi <moi@vermi.fr>
2014-03-31 16:13:23 +0200

    Adding licence information in source code
    MIT Licence is now at the top of each source file

--- PhpCoinD/Network/CoinNetworkSocketManager.php
+++ PhpCoinD/Network/CoinNetworkSocketManager.php
@@ -1,5 +1,30 @@
 <?php
+/**
+ * Copyright (c) 2014 Aurélien RICHAUD
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Created 31/03/14 16:05 by Aurélien RICHAUD
+ */
+
 namespace PhpCoinD\Network;
+
 use Aza\Components\Socket\SocketStream;
 use Exception;
 use Monolog\Logger;
@@ -18,7 +43,6 @@ use PhpCoinD\Protocol\Payload\Addr,
 use PhpCoinD\Protocol\Payload\GetBlocks;
 use PhpCoinD\Protocol\Payload\GetData;
 use PhpCoinD\Protocol\Payload\GetHeaders;
-
 
 /**
  * A default socket manager for coin networks
